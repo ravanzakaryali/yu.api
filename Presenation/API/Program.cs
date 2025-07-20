17c103115cc36745d8d7aac1d341535d1be6b392
Ravan Zakaryalu <ravanzakaryali@gmail.com>
2025-07-20 18:31:27 +0000

    modified

--- Presenation/API/Program.cs
+++ Presenation/API/Program.cs
@@ -84,5 +84,5 @@ app.MapControllers();
 app.UseHttpsRedirection();
 app.UseExceptionHandling();
 app.UseTokenAuthetication();
-app.Urls.Add("http://172.19.8.246:5040");
+// app.Urls.Add("http://172.19.8.246:5040");
 app.Run();
